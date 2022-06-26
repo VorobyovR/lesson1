7449ab5b4691f423abf3b480efa9dd591d552155
VorobyovR <vorobyovromanmail@gmail.com>
2022-06-26 16:16:01 +0300

    example009.1

---
+++ examples/example009.1/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] array = {10, 56, 2, 3, 87, 89, 65, 87};
+int n = array.Length;
+int find = 87;
+int index = 0;
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break; //если необходимо прервать операцию, в данном применре не учитывать такое же число , как ищем
+    }
+    index++;
+}
