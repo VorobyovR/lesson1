2a45dc51690b4f8e2ea6ec909f71d553a1d8d601
VorobyovR <vorobyovromanmail@gmail.com>
2022-06-26 17:04:39 +0300

    example010

---
+++ examples/example010_Metod_Mas/Program.cs
@@ -0,0 +1,48 @@
+﻿void FillArray(int[] collection)
+{
+    int lenght = collection.Length;
+    int index = 0;
+    while (index < lenght)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+
+}
+
+void PrintArray(int[] colect)
+{
+    int count = colect.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(colect[position]);
+        position++;
+    }
+}
+
+int IndexOff(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+    while (index < count)
+    {
+        if (collection[index] == find) 
+        {
+            position = index;
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int[10]; // создать новый массив с количество элементов = 10
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOff(array, 9);
+Console.WriteLine(pos);
+
